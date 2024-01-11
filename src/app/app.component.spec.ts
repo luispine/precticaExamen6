2919272d9633a1916d36fa9bd98ef498a5c1198f
luiVaz <vazquezcedar@outlook.com>
2024-01-10 21:46:42 -0600

    cambios en unittes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -8,7 +8,8 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
-  it('should create the app', () => {
+
+  it('should  be create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
